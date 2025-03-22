9785f132f7930cd28be79548e1eb853d346cb56b
MaryRicucci <maria.ricucci.12@gmail.com>
2025-03-22 11:29:41 +0100

    Update Bomba.java

--- Threads/src/PrimiEsercizi/ThreadBomb/Bomba.java
+++ Threads/src/PrimiEsercizi/ThreadBomb/Bomba.java
@@ -1,31 +1,29 @@
-package PrimiEsercizi.ThreadBomb;
+public class Bomba extends Thread {
+    private String in;
 
-public class Bomba extends Thread{
-    private String in ;
     /*
     Crea un thread che fa un countdown da 10 a 0.
     Se l'utente preme "ENTER" prima che il countdown finisca, stampa "Bomba disinnescata!".
     Se il countdown arriva a 0, stampa "💥 BOOM!".
 
      */
-    public Bomba (String in ) {
-        this.in = in ;
+    public Bomba(String in) {
+        this.in = in;
     }
 
-    public void run () {
-        boolean d = false ;
-        for (int i = 10 ; i>=0 ; i--) {
+    public void run() {
+        boolean d = false;
+        for (int i = 10; i > 0; i--) {
             System.out.println(i);
-            if (in.equals("ENTER")){
-                d = true ;
+            if (in.equals("ENTER")) {
+                d = true;
                 System.out.println("Bomba disinnescata");
-                break ;
+                break;
             }
         }
         if (!d) {
             System.out.println("\uD83D\uDCA5 BOOM");
         }
     }
-
 
 }
