55d729f5c8499453900f935c87b4c631afb1fe74
MaryRicucci <maria.ricucci.12@gmail.com>
2025-03-24 11:44:51 +0100

    Update MainPitStop.java

--- Threads/src/PrimiEsercizi/PitStop/MainPitStop.java
+++ Threads/src/PrimiEsercizi/PitStop/MainPitStop.java
@@ -1,3 +1,13 @@
 public class MainPitStop {
-  public static viid main (String [] args) {
-    Auto 
+  public static void main (String [] args) {
+    Auto a = new Auto ("Lando Norris", "McLaren") ;
+    Auto b = new Auto ("Kimi Antonelli", "Mercedes") ;
+    Auto c = new Auto ("Oliver Bearman", "Haas Ferrari") ;
+
+    a.start () ;
+    b.start () ;
+    c.start () ;
+
+    
+  }
+}
